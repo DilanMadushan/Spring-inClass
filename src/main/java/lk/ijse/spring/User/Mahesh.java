531c698967582a582f652999623ac0cfd38b5827
Dilan Fernando <dilanmadushanka6013@gmail.com>
2024-08-31 10:48:52 +0530

    add Mahesh as user is done

--- src/main/java/lk/ijse/spring/User/Mahesh.java
+++ src/main/java/lk/ijse/spring/User/Mahesh.java
@@ -1,7 +1,14 @@
 package lk.ijse.spring.User;
 
+import lk.ijse.spring.context.BakeryItems;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Mahesh {
+     private BakeryItems bakeryItems;
+
+    public void setBakeryItems(BakeryItems bakeryItems) {
+        this.bakeryItems = bakeryItems;
+    }
 }
